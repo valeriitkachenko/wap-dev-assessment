c736b9465a82a5f16c2a70669744cd162931f3bc
Valeriy Tkachenko <valeriytkachenko757@gmail.com>
2021-07-22 17:22:49 +0300

    Added migrations and models for Orders and Products

---
+++ app/Models/Order.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'external_id',
+        'total',
+        'shipping_total',
+        'create_time',
+        'timezone'
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'create_time' => 'datetime',
+    ];
+}
---
+++ database/migrations/2021_07_22_103228_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('external_id')->unique();
+            $table->decimal('total',8, 2);
+            $table->decimal('shipping_total', 8, 2);
+            $table->dateTime('create_time');
+            $table->string('timezone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
